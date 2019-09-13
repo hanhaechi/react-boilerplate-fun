f4206c5377c6a2f557a1d2f7b4629fb607bb94ea
Modesta Naciute <modesta.naciute@accenture.com>
2019-09-13 16:53:41 -0400

    adds hot reload to preserve state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 
-class App extends Component {
+class App extends React.Component {
   state = {
     count: 0
   };
+
+  increment = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(state => ({ count: state.count - 1 }));
+  };
+
   render() {
+    const { count } = this.state;
     return (
       <div>
-        <h1>Hey, mod!</h1>
-        <h2>Count: {this.state.count}</h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          ADD
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          SUBTRACT
-        </button>
+        <h1>Hello World.</h1>
+        <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
+// same as saying const hotFunc = hot(module)
+// export default hotFunc(App)
