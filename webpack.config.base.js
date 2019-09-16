659a2242b11708ba87e93a35fb67d2836dfe6471
Modesta Naciute <mnaciute@yahoo.com>
2019-09-16 10:42:37 -0400

    target specific browsers with babel polyfill

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,36 +1,50 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "app.bundle.js"
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: [
+                  'last 2 versions',
+                  'not dead',
+                  'not < 2%',
+                  'not ie 11' // removes IE11 saving ~20kb in bundle size
+                ], // same as query 'npx browserslist "last 2 versions, not dead, not < 2%"'
+                useBuiltIns: 'entry'
+              }
+            ],
+            '@babel/preset-react'
+          ],
           plugins: [
-            "react-hot-loader/babel",
-            "@babel/plugin-proposal-class-properties"
+            'react-hot-loader/babel',
+            '@babel/plugin-proposal-class-properties'
           ]
         }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
         exclude: /node_modules/
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: './src/index.html'
     })
   ]
 };
