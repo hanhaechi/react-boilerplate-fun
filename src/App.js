1734615c0d0a818fddddc391a20209e209db157f
Modesta Naciute <modesta.naciute@accenture.com>
2019-09-17 15:50:57 -0400

    final boilerplate

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning'))
-
 class App extends Component {
-  state = {
-    count: 0
-  }
-
-  increment = () => {
-    this.setState(state => ({ count: state.count + 1 }))
-  }
-
-  decrement = () => {
-    this.setState(state => ({ count: state.count - 1 }))
-  }
-
   render() {
-    const { count } = this.state
     return (
       <div>
         <h1>Hello World.</h1>
-        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-        <br />
-        {count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
       </div>
     )
   }
