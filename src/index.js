8e351aba02a0fa6bd9e34e91b0595cfb387f7003
Modesta Naciute <modesta.naciute@accenture.com>
2019-09-17 15:38:52 -0400

    adds DefaultErrorBoundary.js and props validation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@babel/polyfill'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
 import App from './App'
 import './styles.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 )
